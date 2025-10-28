b1686ac0e7a404aae868ebe0df9ff98190170cec
Lucas Grasso <lucasgrassoramos@gmail.com>
2025-10-27 23:07:45 -0300

    add symmetric scheme

--- football_scheduler.py
+++ football_scheduler.py
@@ -7,13 +7,20 @@ class SymetricScheme(Enum):
     FRENCH = 1
     ENGLISH = 2
     INVERTED = 3
-    B2B = 4
+    BACK_TO_BACK = 4
     MIN_MAX = 5
 
 
 class FootballScheduler:
     """
     Football Scheduler class. Initializes the model and decision variables, and handles modeling logic.
+
+    The model is described at:
+    Durán, Guillermo, et al.
+    “Scheduling the South American Qualifiers to the 2018 FIFA World Cup by Integer Programming.”
+    European Journal of Operational Research, vol. 262, no. 3, 1 Nov. 2017, pp. 1109–1115,
+    reader.elsevier.com/reader/sd/pii/S0377221717303909?token=ABD2EAA5C380716EBE2A40E7E9FD273BCD1E3E77BE2AA19DA677BACEBB6711BA08C8C68F8EF491779EF37FEA0268545F,
+    https://doi.org/10.1016/j.ejor.2017.04.043.
 
     Attributes:
                 N: Number of teams
@@ -126,13 +133,13 @@ class FootballScheduler:
             for j in range(self.K):
                 if i == j:
                     continue
-                # C1- every team faces every other team once in the first half
+                # (1) - every team faces every other team once in the first half
                 self.model.addCons(
                     quicksum(self.x[i, j, k] + self.x[j, i, k] for k in range(self.N))
                     == 1,
                     name=f"match_first_half_{i}_{j}",
                 )
-                # C2 - every team faces every other team once in the second half
+                # (2) - every team faces every other team once in the second half
                 self.model.addCons(
                     quicksum(
                         self.x[i, j, k] + self.x[j, i, k]
@@ -141,7 +148,7 @@ class FootballScheduler:
                     == 1,
                     name=f"match_second_half_{i}_{j}",
                 )
-                # C3 - exactly one of the two games is played at home while the other one is played away
+                # (3) - exactly one of the two games is played at home while the other one is played away
                 self.model.addCons(
                     quicksum(self.x[i, j, k] for k in range(self.K)) == 1,
                     name=f"match_second_half_{i}_{j}",
@@ -151,7 +158,7 @@ class FootballScheduler:
         # Compactness constraints
         for j in range(self.N):
             for k in range(self.K):
-                # C4 - all teams must play one match in each round.
+                # (4) - all teams must play one match in each round.
                 self.model.addCons(
                     quicksum(
                         self.x[i, j, k] + self.x[j, i, k]
@@ -167,7 +174,7 @@ class FootballScheduler:
         for i in [x for x in range(self.N) if x not in self.I_s]:
             for k in range(self.K):
                 for j in self.I_s:
-                    # C5 - No non-top team be required to play against any of the top teams in consecutive matches.
+                    # (5) - No non-top team be required to play against any of the top teams in consecutive matches.
                     self.model.addCons(
                         quicksum(
                             self.x[i, j, k]
@@ -182,7 +189,7 @@ class FootballScheduler:
     def __instance_balance_constraints(self):
         # Balance constraints
         for i in range(self.N):
-            # C6 - Each team has bewteen N/2-1 and N/2 H-A sequences in double rounds.
+            # (6) - Each team has bewteen N/2-1 and N/2 H-A sequences in double rounds.
             self.model.addCons(
                 quicksum(self.y[i, k] for k in range(1, self.K, 2))
                 >= (self.N // 2) - 1,
@@ -194,7 +201,7 @@ class FootballScheduler:
             )
 
             for k in range(1, self.K, 2):
-                # C7 - Teams should not play consecutive home or away matches in double rounds.
+                # (7) - Teams should not play consecutive home or away matches in double rounds.
                 self.model.addCons(
                     quicksum(
                         self.x[i, j, k] + self.x[i, j, k + 1]
@@ -204,13 +211,13 @@ class FootballScheduler:
                     <= 1 + self.y[i, k],
                     name=f"HA_{i}_{k}",
                 )
-                # C8 -
+                # (8) -
                 self.model.addCons(
                     quicksum(self.x[i, j, k] for j in range(self.N) if j != i)
                     >= self.y[i, k],
                     name=f"c8_{i}_{k}",
                 )
-                # C9 -
+                # (9) -
                 self.model.addCons(
                     quicksum(self.x[i, j, k + 1] for j in range(self.N) if j != i)
                     >= self.y[i, k],
@@ -221,7 +228,7 @@ class FootballScheduler:
         # Aux constraints
         for i in range(self.N):
             for k in range(1, self.K, 2):
-                # C10 - Teams should not have two consecutive away breaks
+                # (10) - Teams should not have two consecutive away breaks
                 self.model.addCons(
                     quicksum(
                         self.x[i, j, k] + self.x[i, j, k + 1]
@@ -231,13 +238,13 @@ class FootballScheduler:
                     <= 1 + self.w[i, k],
                     name=f"AB_{i}_{k}",
                 )
-                # C11 -
+                # (11) -
                 self.model.addCons(
                     quicksum(self.x[i, j, k] for j in range(self.N) if j != i)
                     >= self.w[i, k],
                     name=f"c11_{i}_{k}",
                 )
-                # C12 -
+                # (12) -
                 self.model.addCons(
                     quicksum(self.x[i, j, k + 1] for j in range(self.N) if j != i)
                     >= self.w[i, k],
@@ -245,13 +252,114 @@ class FootballScheduler:
                 )
 
     def __instance_symmetric_scheme_constraints(self):
-        pass
+        if self.scheme == SymetricScheme.MIRRORED:
+            for i in range(self.N):
+                for j in range(self.N):
+                    if i == j:
+                        continue
+                    for k in range(self.N - 1):
+                        # (14) - Mirror scheme constraint
+                        self.model.addCons(
+                            self.x[i, j, k] == self.x[j, i, k + self.N - 1],
+                            f"mirrored_{i}_{j}_{k}",
+                        )
+
+        elif self.scheme == SymetricScheme.FRENCH:
+            for i in range(self.N):
+                for j in range(self.N):
+                    if i == j:
+                        continue
+                    # (15) - French scheme constraint 1
+                    self.model.addCons(
+                        self.x[i, j, 0] == self.x[j, i, 2 * self.N - 3],
+                        f"french_1_{i}_{j}_{k}",
+                    )
+                    for k in range(1, self.N - 1):
+                        # (15) - French scheme constraint 2
+                        self.model.addCons(
+                            self.x[i, j, k] == self.x[j, i, k + self.N - 2],
+                            f"french_2_{i}_{j}_{k}",
+                        )
+
+        elif self.scheme == SymetricScheme.ENGLISH:
+            for i in range(self.N):
+                for j in range(self.N):
+                    if i == j:
+                        continue
+                    # (16) - English scheme constraint 1
+                    self.model.addCons(
+                        self.x[i, j, self.N - 2] == self.x[j, i, self.N - 1],
+                        f"english_1_{i}_{j}_{k}",
+                    )
+                    for k in range(1, self.N - 2):
+                        # (16) - English scheme constraint 2
+                        self.model.addCons(
+                            self.x[i, j, k] == self.x[j, i, k + self.N],
+                            f"english_2_{i}_{j}_{k}",
+                        )
+
+        elif self.scheme == SymetricScheme.INVERTED:
+            for i in range(self.N):
+                for j in range(self.N):
+                    if i == j:
+                        continue
+                    for k in range(self.N - 1):
+                        # (17) - Inverted scheme constraint
+                        self.model.addCons(
+                            self.x[i, j, k] == self.x[j, i, 2 * self.N - 1 - k],
+                            f"inverted_{i}_{j}_{k}",
+                        )
+
+        elif self.scheme == SymetricScheme.BACK_TO_BACK:
+            for i in range(self.N):
+                for j in range(self.N):
+                    if i == j:
+                        continue
+                    for k in range(1, self.N - 1, 2):
+                        # (18) - Back to back scheme constraint
+                        self.model.addCons(
+                            self.x[i, j, k] == self.x[j, i, k + 1],
+                            f"back_to_back_{i}_{j}_{k}",
+                        )
+
+        elif self.scheme == SymetricScheme.MIN_MAX:
+            for i in range(self.N):
+                for j in range(self.N):
+                    if i == j:
+                        continue
+                    for k in range(0, self.K - self.c - 1):
+                        # (19) - Min max scheme constraint 1
+                        self.model.addCons(
+                            quicksum(
+                                self.x[i, j, q] + self.x[j, i, q]
+                                for q in range(k, k + self.c + 1)
+                            )
+                            <= 1,
+                            f"min_max_1_{i}_{j}_{k}",
+                        )
+                    for k in range(0, self.K):
+                        # (19) - Min max scheme constraint 2
+                        self.model.addCons(
+                            quicksum(
+                                self.x[i, j, q]
+                                for q in range(
+                                    max(k - self.d, 0),
+                                    min(k + self.d, 2 * (self.N - 1)),
+                                )
+                                if q != k
+                            )
+                            >= self.x[j, i, k],
+                            f"min_max_2_{i}_{j}_{k}",
+                        )
+
+        else:
+            pass
 
     def __set_objective(self):
         """
         Sets the objective function.
         """
-        # O - Minimize the total number ofaway breaks within double rounds across all teams.
+        # (13) - Minimize the total number ofaway breaks within double rounds across all teams.
         self.model.setObjective(
             quicksum(self.w[i, k] for k in range(1, self.K, 2) for i in range(self.N)),
             sense="minimize",
